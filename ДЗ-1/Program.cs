41c7d7d7de599a3131617c496b18f35072ac18bc
Timur <timuryuta@gmail.com>
2022-06-26 21:19:07 +0300

    created task-3

--- ДЗ-1/Program.cs
+++ ДЗ-1/Program.cs
@@ -16,21 +16,31 @@
 
 // Задача 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
 
-Console.Write("Введите число: a = " );
-int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: a = " );
+// int a = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число: b = " );
-int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: b = " );
+// int b = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число: c = " );
-int c = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: c = " );
+// int c = Convert.ToInt32(Console.ReadLine());
 
-int max = a;
+// int max = a;
 
-if (b > a) max = b;
-if (c > a) max = c;
-if (b > c) max = b;
+// if (b > a) max = b;
+// if (c > a) max = c;
+// if (b > c) max = b;
 
-Console.Write("max = " + max);
+// Console.Write("max = " + max);
 
 
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.Write("Введите число: " );
+int number = Convert.ToInt32(Console.ReadLine());
+int div = number / 2;
+
+if (number % 2 == 0)
+    Console.Write("ДА, число чётное!");
+else
+    Console.Write("НЕТ, число нечётное!");
