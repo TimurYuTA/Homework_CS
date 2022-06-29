d24092f40758905d01ff4bbf83510f81104adcb2
Timur <timuryuta@gmail.com>
2022-06-30 00:21:36 +0300

    ДЗ-2. Задача 1

--- ДЗ-1/Program.cs
+++ ДЗ-1/Program.cs
@@ -1,4 +1,6 @@
-﻿// Задача 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// ДЗ-1
+
+// Задача 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 // Console.Write("Введите число: a = " );
 // int a = Convert.ToInt32(Console.ReadLine());
@@ -47,15 +49,34 @@
 
 // Задача 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
 
-Console.Write("Введите число: " );
+// Console.Write("Введите число: " );
+// int number = Convert.ToInt32(Console.ReadLine());
+// int step = 2;
+
+// if (number < 2)
+//     Console.Write("Чётных чисел нет!");
+
+// while (step <= number)
+// {
+//     Console.Write(step + "   ");
+//     step += 2;
+// }
+
+
+// ДЗ-2
+
+// Задача 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Write("Введите трёхзначное число: " );
 int number = Convert.ToInt32(Console.ReadLine());
-int step = 2;
 
-if (number < 2)
-    Console.Write("Чётных чисел нет!");
-
-while (step <= number)
-{
-    Console.Write(step + "   ");
-    step += 2;
+if (number > -1000 & number < -99 ^ number > 99 & number < 1000 ){
+    int tens = number % 100 / 10;
+    if (tens < 0){
+        tens = tens * -1;
+    }
+    Console.Write("Вторая цифра числа " + number + " -> " + tens);
+}
+else{
+    Console.Write("Число " + number + " не является трёхзначным!");
 }
